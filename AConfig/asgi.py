c4e15a4b91eef94dc103dd91ac033bda71583a9d
Sam231221 <rasengankid999@gmail.com>
2022-11-08 19:02:35 +0545

    asgi updated

--- AConfig/asgi.py
+++ AConfig/asgi.py
@@ -8,13 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
 import MChat.routing
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AConfig.settings')
-
+django.setup()
 application = ProtocolTypeRouter({
     #un http protocol as it is
     'http': get_asgi_application(),
