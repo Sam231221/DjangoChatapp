0f7bfef38f1509ffcb219b8fb7e105536c7a0d84
Sam231221 <rasengankid999@gmail.com>
2022-11-08 21:08:58 +0545

    procfile updated v4

--- AConfig/routing.py
+++ AConfig/routing.py
@@ -1,7 +1,10 @@
 import MChat.routing
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+
 application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
             MChat.routing.websocket_urlpatterns
